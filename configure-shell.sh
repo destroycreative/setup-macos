4c784744a210a89d7049b16d46459186a27b2086
Leslie Doherty <leslie.doherty@salesforce.com>
2021-05-06 16:41:23 -0700

    （○□○）bashrc is globally ignored. Update shell to rename on move.

--- configure-shell.sh
+++ configure-shell.sh
@@ -5,7 +5,7 @@ echo '⚡️ Configuring the shell'
 chsh -s /bin/bash
 
 ## Move the .bashrc and .bash_profile to user root.
-mv ./files/.bashrc ~/.bashrc
+mv ./files/bashrc.text ~/.bashrc
 mv ./files/.bash_profile ~/.bash_profile
 
 ## Kill the zsh warning.
