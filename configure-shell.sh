2e2bce92102c5f0d34377dfe26b9f5cf32ea3767
Leslie Doherty <leslie.doherty@salesforce.com>
2021-05-06 16:34:44 -0700

    Configure shell for bash. Move bash files to user directory.

--- configure-shell.sh
+++ configure-shell.sh
@@ -2,4 +2,14 @@
 
 echo ''
 echo '⚡️ Configuring the shell'
-chsh -s /bin/zsh
+chsh -s /bin/bash
+
+## Move the .bashrc and .bash_profile to user root.
+mv ./files/.bashrc ~/.bashrc
+mv ./files/.bash_profile ~/.bash_profile
+
+## Kill the zsh warning.
+export BASH_SILENCE_DEPRECATION_WARNING=1 >> ~/.bashrc
+
+## Restart bash.
+source ~/.bash_profile
