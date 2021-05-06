78aed92d28196edb7ba7093a0f757cdb3d9111d0
Leslie Doherty <leslie.doherty@salesforce.com>
2021-05-06 16:33:12 -0700

    Show hidden files, bump. memory limits for PHP.

--- configure-macos.sh
+++ configure-macos.sh
@@ -3,9 +3,6 @@
 echo ''
 echo '⚡️ Configuring macOS'
 
-## Set press and hold to repeat a key, rather than displaying the accent-picker.
-defaults write -g ApplePressAndHoldEnabled -bool false
-
 ## Use AirDrop over every interface.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
@@ -15,8 +12,8 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 ## Show the ~/Library folder.
 chflags nohidden ~/Library
 
-## Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
+## Show hidden files.
+defaults write com.apple.finder AppleShowAllFiles YES
 
 ## Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
@@ -29,10 +26,15 @@ defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool 
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-## Style the Dock.
-defaults write com.apple.dock orientation -string left
-defaults write com.apple.dock magnification -bool false
-defaults write com.apple.dock static-only -bool true
-defaults write com.apple.dock autohide -bool true
-defaults write com.apple.dock mineffect -string scale
-defaults write com.apple.dock tilesize -int 16
+## Bump up maxfile and maxproc limit on Mac OS.
+curl -O https://gist.githubusercontent.com/tombigel/d503800a282fcadbee14b537735d202c/raw/ed73cacf82906fdde59976a0c8248cce8b44f906/limit.maxfiles.plist
+curl -O https://gist.githubusercontent.com/tombigel/d503800a282fcadbee14b537735d202c/raw/ed73cacf82906fdde59976a0c8248cce8b44f906/limit.maxproc.plist
+ 
+sudo mv limit.maxfiles.plist /Library/LaunchDaemons
+sudo mv limit.maxproc.plist /Library/LaunchDaemons
+ 
+sudo chown root:wheel /Library/LaunchDaemons/limit.maxfiles.plist
+sudo chown root:wheel /Library/LaunchDaemons/limit.maxproc.plist
+ 
+sudo launchctl load -w /Library/LaunchDaemons/limit.maxfiles.plist
+sudo launchctl load -w /Library/LaunchDaemons/limit.maxproc.plist
