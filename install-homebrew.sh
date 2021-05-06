3c2af652f23e3144140b5be14590e0003dd9f9f1
Leslie Doherty <leslie.doherty@salesforce.com>
2021-05-06 16:36:17 -0700

    Consolidated Homebrew installs

--- install-homebrew.sh
+++ install-homebrew.sh
@@ -2,4 +2,16 @@
 
 echo ''
 echo '⚡️ Installing Homebrew'
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew update
+
+## Brew services integrates Homebrew formulae with macOS' launchctl manager.
+brew tap homebrew/services
+
+## Install dns masq seperately due to error in valet install.
+brew install dnsmasq
+
+
+## Install NVM
+brew install nvm
